cd815ba310fd791f03eae597f4f7f4040ddb20ee
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-09-24 17:30:19 -0400

    :done:

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const app = "I don't do much."
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+  this.serve = function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
+}
+
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber);
+}
+function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+
+var sally = new Customer("Sally", "4");
+
+//schedule table visit
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000);
